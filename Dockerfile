fd8dfe707622ecf926107862f032318f883c7c9f
jdparkinson93 <32483798+jdparkinson93@users.noreply.github.com>
2018-02-19 12:54:14 +0000

    Improved config setup with a .bashrc generator

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,13 @@ FROM golang:latest
 
 MAINTAINER jdparkinson93 <32483798+jdparkinson93@users.noreply.github.com>
 
-ARG HOME=/.
-ENV HOME=$HOME GOPATH=$HOME/git
+ENV HOME=/
 ADD .bashrc $HOME
 ADD .vimrc $HOME
 
-RUN /bin/bash -c 'source $HOME/.bashrc && \
-	apt-get update && apt-get install -y vim libtool && \
+RUN /bin/bash -c 'source $HOME/.bashrc'
+
+RUN /bin/bash -c 'apt-get update && apt-get install -y vim libtool && \
 	curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
 	git clone https://github.com/fatih/vim-go.git $HOME/.vim/plugged/vim-go && \
 	vim +PlugInstall +q +q'
--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-IMAGE_NAME="vim-go"
 
+## Create .bashrc file from template
+echo "$(cat bashrc.template)" > ._bashrc
+
+## Define env variables for container using local variables
+echo "export http_proxy=$http_proxy" >> ._bashrc
+echo "export https_proxy=$https_proxy" >> ._bashrc
+echo "export no_proxy=$no_proxy" >> ._bashrc
+
+GOPATH=/home/ape/jparkins/git
+GOPATH_DOCKER=$(echo "${GOPATH#$HOME}")
+echo "export GOPATH=$GOPATH_DOCKER" >> ._bashrc
+
+IMAGE_NAME="vim-go"
 ## Build image
 docker build -t $IMAGE_NAME .
 
-GOPATH_TARGET=/git
-
 ## Run container
-docker run -it -v $GOPATH:$GOPATH_TARGET $IMAGE_NAME
+docker run -it -v $GOPATH:$GOPATH_DOCKER $IMAGE_NAME
