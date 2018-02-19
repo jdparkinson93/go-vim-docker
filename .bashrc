b3590d0663eb1d766a9a5cbe777298108965bbbd
jdparkinson93 <32483798+jdparkinson93@users.noreply.github.com>
2018-02-19 13:00:08 +0000

    Further improved bashrc generator to set git config user

--- .bashrc
+++ .bashrc
@@ -3,14 +3,9 @@ if [[ -t 1 ]]; then
         PS1="$(put setaf 10)\u@\h$(put setaf 7):$(put setaf 4)\w$(put sgr0)\$ "
         unset put
 fi
-
-echo HOME=$HOME
-echo GOPATH=$GOPATH
-
-
-user="jdparkinson93"
-email="32483798+jdparkinson93@users.noreply.github.com"
-
-git config --global user.name $user
-git config --global user.email $email
-git config --global -l
+export http_proxy=http://ukproxy.lseg.stockex.local:8082
+export https_proxy=http://ukproxy.lseg.stockex.local:8082
+export no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,.local,192.168.30.100
+export GOPATH=/git
+git config --global user.name jdparkinson93
+git config --global user.email 32483798+jdparkinson93@users.noreply.github.com
--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+bashrc=".bashrc"
+
 ## Create .bashrc file from template
-echo "$(cat bashrc.template)" > ._bashrc
+echo "$(cat bashrc.template)" > $bashrc
 
 ## Define env variables for container using local variables
-echo "export http_proxy=$http_proxy" >> ._bashrc
-echo "export https_proxy=$https_proxy" >> ._bashrc
-echo "export no_proxy=$no_proxy" >> ._bashrc
+echo "export http_proxy=$http_proxy" >> $bashrc
+echo "export https_proxy=$https_proxy" >> $bashrc
+echo "export no_proxy=$no_proxy" >> $bashrc
 
 GOPATH=/home/ape/jparkins/git
 GOPATH_DOCKER=$(echo "${GOPATH#$HOME}")
-echo "export GOPATH=$GOPATH_DOCKER" >> ._bashrc
+echo "export GOPATH=$GOPATH_DOCKER" >> $bashrc
+
+## Set git config
+echo "git config --global user.name $(git config --global user.name)" >> $bashrc
+echo "git config --global user.email $(git config --global user.email)" >> $bashrc
 
 IMAGE_NAME="vim-go"
 ## Build image
